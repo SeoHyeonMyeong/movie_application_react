0576707c444711e39648f3d38e050589ae2274ad
SeoHyeonMyeong <gusaud2199@naver.com>
2022-01-19 16:39:53 +0900

    Update: use 'useEffect' function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+  
+  useEffect(() => {
+    console.log("i run only once.");
+  }, []);
+  
   return (
     <div>
       <h1 className={styles.title}>{counter}</h1>
