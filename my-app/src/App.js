f1e4cb50d2a9a0636a906d2059b42c4787d14019
SeoHyeonMyeong <gusaud2199@naver.com>
2022-01-19 16:55:38 +0900

    Update: use 'useEffect' function with deps

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,14 +4,33 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  
+  const onChange = (event) => setKeyword(event.target.value);
+
+  // 초기에 한번만 실행
   useEffect(() => {
-    console.log("i run only once.");
+    console.log("API 호출");
   }, []);
-  
+
+  // 키워드 변경시 실행
+  useEffect(() =>{
+    console.log("검색: ", keyword);
+  }, [keyword]);
+
+  // 카운터 변경시 실행
+  useEffect(() => {
+    console.log("숫자 변경: ", counter);
+  }, [counter]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="입력..."
+      />
       <h1 className={styles.title}>{counter}</h1>
       <Button text={"First Button"} onClick={onClick}/>
     </div>
