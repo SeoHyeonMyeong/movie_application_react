d733615be895c8911b8219b9fa771d4685033caa
SeoHyeonMyeong <gusaud2199@naver.com>
2022-02-07 19:22:43 +0900

    Update : use "react-router-dom" and make app route "/", "/movie"

--- my-app/src/components/MovieCard.js
+++ my-app/src/components/MovieCard.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function MovieCard({ coverImage, title, genres, summary }) {
     return (
         <div >
             <img alt={title} src={coverImage}/>
-            <h2>{title}</h2>
+            <h2>
+                <Link to="/movie">{title}</Link>
+            </h2>
             <ul>
                 {genres.map(genre => <li key={genre}>{genre}</li>)}
             </ul>
