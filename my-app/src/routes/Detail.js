88dd3add61f9690a37d92b58a9e4e1c75877aa94
SeoHyeonMyeong <gusaud2199@naver.com>
2022-02-10 17:29:08 +0900

    Update: remove Junk component to Junk.js routes

--- my-app/src/routes/Detail.js
+++ my-app/src/routes/Detail.js
@@ -5,7 +5,7 @@ function Detail() {
     const { id } = useParams();
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState([]);
-    console.log(id);
+
     const getMovie = async () => {
         const json = await (
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
@@ -13,9 +13,11 @@ function Detail() {
         setData(json.data.movie);
         setLoading(true);
     };
+
     useEffect(() => {
         getMovie();
     }, []);
+
     return (
     <div>
         <h1>Detail</h1>
--- my-app/src/routes/Home.js
+++ my-app/src/routes/Home.js
@@ -1,51 +1,11 @@
 import { useState, useEffect } from "react";
-import Button from "../components/Button";
 import MovieApp from "../components/MovieApp";
-import Todo from "../components/Todo";
-import CoinTracker from "../components/CoinTracker";
 import styles from "./Home.module.css";
 
 function Home() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const [showing, setShowing] = useState("");
-    const onClickShow = () => setShowing((prev) => !prev);
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-
-    // 초기에 한번만 실행
-    useEffect(() => {
-        console.log("API 호출");
-    }, []);
-
-    // 키워드 변경시 실행
-    useEffect(() =>{
-        console.log("검색: ", keyword);
-    }, [keyword]);
-
-    // 카운터 변경시 실행
-    useEffect(() => {
-        console.log("숫자 변경: ", counter);
-    }, [counter]);
-
     return (
         <div>
-        <input
-            value={keyword}
-            onChange={onChange}
-            type="text"
-            placeholder="입력..."
-        />
-        <h1 className={styles.title}>{counter}</h1>
-        <Button text={"First Button"} onClick={onClick}/>
-        <hr/>
-        <Button text={showing ? "숨김" : "보이기"} onClick={onClickShow}/>
-        <hr/>
-        <Todo/>
-        <hr/>
-        <CoinTracker />
-        <hr/>
-        <MovieApp/>
+            <MovieApp/>
         </div>
     );
 }
